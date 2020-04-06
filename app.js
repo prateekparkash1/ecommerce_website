fb41c3d4361acbf900193c08060f40f8679d9e79
prateekparkash1 <prateekparkash1@gmail.com>
2020-04-06 16:00:23 +0530

    first commit

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
 require("dotenv").config();
 // import routes
+const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+
+
 
 // app
 const app = express();
@@ -27,8 +31,9 @@ app.use(cookieParser());
 app.use(expressValidator());
 
 // routes middleware
+app.use("/api", authRoutes);
 app.use("/api", userRoutes);
-
+app.use("/api", categoryRoutes);
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
-const { signup, signin, signout, requireSignin } = require("../controllers/user");
-const { userSignupValidator } = require("../validator");
+const { userById } = require("../controllers/user");
 
-router.post("/signup", userSignupValidator, signup);
-router.post("/signin", signin);
-router.get("/signout", signout);
-// router.get("/hello",requireSignin,(req,res)=>  {
-// 	res.send('hello there')
-// });
+router.get('/secret/:userId',requireSignin,isAuth,(req,res)=>{
+	res.json({
+		user:req.profile
+	});
+});
 
+router.param('userId',userById);
 
 module.exports = router;

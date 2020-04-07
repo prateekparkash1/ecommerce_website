77e4830ee9ca003c6cbd750cd61e484f6e949d58
prateekparkash1 <prateekparkash1@gmail.com>
2020-04-07 17:53:10 +0530

    added product routes, serach, category search

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,72 +46,31 @@ exports.signin = (req, res) => {
     });
 };
 
+exports.signout = (req, res) => {
+    res.clearCookie("t");
+    res.json({ message: "Signout success" });
+};
 
-
-exports.signout = (req,res) => {
-    res.clearCookie('t')
-    res.json({message:"Signout success"});
-}
-
-exports.requireSignin= expressJwt({
+exports.requireSignin = expressJwt({
     secret: process.env.JWT_SECRET,
-    userProperty:"auth"
+    userProperty: "auth"
 });
 
-
-
-exports.isAuth = (req,res,next) =>{
-    let user=req.profile && req.auth && req.profile._id==req.auth._id
-    if(!user){
-        return res.status(400).json({
-            error: 'Access denied'
-        });
-    }
-    next();
-};
-
-
-exports.isAdmin= (req,res,next)=>{
-    if(req.profile.role===0){
+exports.isAuth = (req, res, next) => {
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
+    if (!user) {
         return res.status(403).json({
-            error:'Admin resource! access denied'
+            error: "Access denied"
         });
     }
     next();
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+        return res.status(403).json({
+            error: "Admin resourse! Access denied"
+        });
+    }
+    next();
+};
